e1e6d64e274e0b819a96f7b5852fd9b9e5fe4c54
Jamie Guthrie <jamie.guthrie@Y01672.local>
2017-10-11 14:09:16 +0100

    Added sdk_identifier value to configuration.rb

--- lib/yoti/configuration.rb
+++ lib/yoti/configuration.rb
@@ -8,6 +8,7 @@ module Yoti
       @client_sdk_id = ''
       @key_file_path = ''
       @key = ''
+      @sdk = 'Ruby'
       @api_url = 'https://api.yoti.com'
       @api_port = 443
       @api_version = 'v1'

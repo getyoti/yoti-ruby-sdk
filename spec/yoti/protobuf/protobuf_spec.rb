b9368bc1238f834b6cbcc1ce2b3b96c6f388a163
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2017-10-24 15:39:13 +0100

    [SDK-136]: Update specs to cover protobuf image content type

--- spec/yoti/protobuf/protobuf_spec.rb
+++ spec/yoti/protobuf/protobuf_spec.rb
@@ -82,4 +82,24 @@ describe 'Yoti::Protobuf' do
       end
     end
   end
+
+  describe '.image_uri_based_on_content_type' do
+    let(:value) { 'test string' }
+    subject { Yoti::Protobuf.image_uri_based_on_content_type(value, content_type) }
+
+    context 'when the content type is 2' do
+      let(:content_type) { 2 }
+      it 'encodes the string to JPEG format' do
+        is_expected.to eql('data:image/jpeg;base64,dGVzdCBzdHJpbmc=')
+      end
+    end
+
+    context 'when the content type is 4' do
+      let(:content_type) { 4 }
+
+      it 'encodes the string to PNG format' do
+        is_expected.to eql('data:image/png;base64,dGVzdCBzdHJpbmc=')
+      end
+    end
+  end
 end

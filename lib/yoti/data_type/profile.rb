e768c7708e231353eab806c40421ebdca1dd61b0
Moussa Sidibe <moussa.sidibe@yoti.com>
2018-08-02 11:00:44 +0100

    SDK-489: Add anchor.processor class

--- lib/yoti/data_type/profile.rb
+++ lib/yoti/data_type/profile.rb
@@ -13,11 +13,43 @@ module Yoti
 
     public
         def family_name
-            return self.get_attribute('family_name')
+            return get_attribute('family_name')
         end
 
         def given_names
-            return self.get_attribute('given_names')
+            return get_attribute('given_names')
+        end
+
+        def full_name
+            return get_attribute('full_name')
+        end
+
+        def phone_number
+            return get_attribute('phone_number')
+        end
+
+        def email_address
+            return get_attribute('email_address')
+        end
+
+        def date_of_birth
+            return get_attribute('date_of_birth')
+        end
+
+        def postal_address
+            return get_attribute('postal_address')
+        end
+
+        def gender
+            return get_attribute('gender')
+        end
+
+        def nationality
+            return get_attribute('nationality')
+        end
+
+        def structured_postal_address
+            return get_attribute('structured_postal_address')
         end
 
         def get_attribute(attr_name)

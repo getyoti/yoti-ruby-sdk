d97b6d4148e58fe3135cbc5e0051f90bfb5bf909
David Grayston <davidgrayston@googlemail.com>
2019-03-26 11:53:27 +0000

    SDK-873: Use constant in test context

--- spec/yoti/protobuf/protobuf_spec.rb
+++ spec/yoti/protobuf/protobuf_spec.rb
@@ -86,7 +86,7 @@ describe 'Yoti::Protobuf' do
       end
     end
 
-    context 'when the content type is 5' do
+    context 'when the content type is JSON' do
       json_string = '{"test": "json string"}'
       let(:content_type) { :JSON }
       let(:value) { json_string }

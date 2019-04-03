71190b292fad5f4cc5abe00daac2b7583a5c112e
David Grayston <davidgrayston@googlemail.com>
2019-04-03 16:43:54 +0100

    SDK-898: Test log messages

--- lib/yoti/util/log.rb
+++ lib/yoti/util/log.rb
@@ -4,7 +4,17 @@ module Yoti
   module Log
     class << self
       def logger
-        @logger ||= Logger.new(STDOUT)
+        @logger || create_logger(STDOUT)
+      end
+
+      def output(output_stream)
+        create_logger(output_stream)
+      end
+
+      private
+
+      def create_logger(output_stream)
+        @logger = Logger.new(output_stream)
       end
     end
   end

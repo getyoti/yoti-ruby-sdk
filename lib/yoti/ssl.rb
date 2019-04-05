c568348f2f1a5d830437f928a294a7828bba18c3
David Grayston <davidgrayston@googlemail.com>
2019-04-05 12:13:30 +0100

    SDK-593: Coding standards

--- lib/yoti/ssl.rb
+++ lib/yoti/ssl.rb
@@ -26,8 +26,8 @@ module Yoti
 
         begin
           private_key.private_decrypt(Base64.urlsafe_decode64(encrypted_connect_token))
-        rescue StandardError => error
-          raise SslError, "Could not decrypt token. #{error}"
+        rescue StandardError => e
+          raise SslError, "Could not decrypt token. #{e}"
         end
       end
 
@@ -63,8 +63,8 @@ module Yoti
 
       def private_key
         @private_key ||= OpenSSL::PKey::RSA.new(pem)
-      rescue StandardError => error
-        raise SslError, "The secure key is invalid. #{error}"
+      rescue StandardError => e
+        raise SslError, "The secure key is invalid. #{e}"
       end
     end
   end

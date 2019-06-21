61bb3b523bd6fb364a9da32c8b2dc189de71384d
David Grayston <davidgrayston@googlemail.com>
2019-06-21 12:35:33 +0100

    SDK-1017: Add attributes attr_reader to BaseProfile

--- spec/yoti/data_type/application_profile_spec.rb
+++ spec/yoti/data_type/application_profile_spec.rb
@@ -1,32 +1,53 @@
 describe 'Yoti::ApplicationProfile' do
-  profile_data = { Yoti::Attribute::APPLICATION_NAME => 'test_name',
-                   Yoti::Attribute::APPLICATION_URL => 'test_url',
-                   Yoti::Attribute::APPLICATION_LOGO => 'test_logo',
-                   Yoti::Attribute::APPLICATION_RECEIPT_BGCOLOR => 'test_receipt_bgcolor' }
+  profile_values = { Yoti::Attribute::APPLICATION_NAME => 'test_name',
+                     Yoti::Attribute::APPLICATION_URL => 'test_url',
+                     Yoti::Attribute::APPLICATION_LOGO => 'test_logo',
+                     Yoti::Attribute::APPLICATION_RECEIPT_BGCOLOR => 'test_receipt_bgcolor' }
+
+  profile_data = profile_values.map do |name, value|
+    [name, Yoti::Attribute.new(name, value, {}, {})]
+  end.to_h
 
   profile = Yoti::ApplicationProfile.new(profile_data)
 
   describe '.name' do
     it 'should return test_name' do
-      expect(profile.name).to eql('test_name')
+      expect(profile.name.value).to eql('test_name')
     end
   end
 
   describe '.url' do
     it 'should return test_url' do
-      expect(profile.url).to eql('test_url')
+      expect(profile.url.value).to eql('test_url')
     end
   end
 
   describe '.logo' do
     it 'should return test_logo' do
-      expect(profile.logo).to eql('test_logo')
+      expect(profile.logo.value).to eql('test_logo')
     end
   end
 
   describe '.receipt_bgcolor' do
     it 'should return test_receipt_bgcolor' do
-      expect(profile.receipt_bgcolor).to eql('test_receipt_bgcolor')
+      expect(profile.receipt_bgcolor.value).to eql('test_receipt_bgcolor')
+    end
+  end
+
+  describe '.get_attribute' do
+    profile_values.each do |key, value|
+      it "'#{key}' should return '#{value}''" do
+        expect(profile.get_attribute(key).value).to eql(value)
+      end
+    end
+  end
+
+  describe '.attributes' do
+    it 'should return all attributes' do
+      expect(profile.attributes.length).to eql(4)
+      profile_values.each do |key, value|
+        expect(profile.attributes[key].value).to eql(value)
+      end
     end
   end
 end

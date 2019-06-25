5e70a6f80bd1e9f656e5ad910fa3cc1c4ea89e34
David Grayston <davidgrayston@googlemail.com>
2019-06-25 15:28:04 +0100

    SDK-713: Add anchors getter to attribute

--- spec/yoti/data_type/attribute_spec.rb
+++ spec/yoti/data_type/attribute_spec.rb
@@ -8,9 +8,12 @@ end
 describe 'Yoti::Attribute' do
   dl_source_anchor = File.read('spec/sample-data/anchors/dl-source.txt')
   dl_verifier_anchor = File.read('spec/sample-data/anchors/dl-verifier.txt')
+  unknown_anchor = File.read('spec/sample-data/anchors/unknown-anchor.txt')
   dl_sources_list = process_anchors_list(dl_source_anchor)['sources']
-  dl_verifiers = process_anchors_list(dl_verifier_anchor)['verifiers']
-  yoti_attribute = Yoti::Attribute.new('test_given_names', 'Test GIVEN NAMES', dl_sources_list, dl_verifiers)
+  dl_verifiers_list = process_anchors_list(dl_verifier_anchor)['verifiers']
+  unknown_anchors_list = process_anchors_list(unknown_anchor)['unknown']
+  anchors_list = dl_sources_list + dl_verifiers_list + unknown_anchors_list
+  yoti_attribute = Yoti::Attribute.new('test_given_names', 'Test GIVEN NAMES', dl_sources_list, dl_verifiers_list, anchors_list)
 
   describe '.name' do
     it 'should return test_given_names' do
@@ -37,6 +40,18 @@ describe 'Yoti::Attribute' do
 
     it 'should return YOTI_ADMIN as value' do
       expect(first_verifier.value).to eql('YOTI_ADMIN')
+    end
+  end
+
+  describe '.anchors' do
+    it 'should return a list of all anchors' do
+      anchors = yoti_attribute.anchors
+      anchors.each do |anchor|
+        expect(anchor).to be_a(Yoti::Anchor)
+      end
+      expect(anchors[0].type).to eql('SOURCE')
+      expect(anchors[1].type).to eql('VERIFIER')
+      expect(anchors[2].type).to eql('UNKNOWN')
     end
   end
 end

c08ae26f7bbda078ac82a9628a463d1670f35697
Emma Smith <Emma.Smith@Yoti.com>
2019-09-23 15:48:38 +0100

    SDK-726: Implement share_url methods creating dynamic share

--- spec/yoti/dynamic_share_service/share_url_spec.rb
+++ spec/yoti/dynamic_share_service/share_url_spec.rb
@@ -22,8 +22,7 @@ describe 'Yoti::DynamicSharingService::create_share_url' do
 
   context 'when the scenario is valid' do
     before(:context) do
-      stub_api_requests_v1(:any, share_url, '[a-zA-Z]*', 201)
-    end
+      stub_api_requests_v1(:any, 'share_url', '[a-zA-Z]*', 201)
     end
     it 'generates a share url' do
       expect(share.share_url).to eql 'https://code.yoti.com/forfhq3peurij4ihroiehg4jgiej'
@@ -32,18 +31,38 @@ describe 'Yoti::DynamicSharingService::create_share_url' do
   end
 
   context 'when the scenario is invalid' do
-    it 'returns an invalid data error' do
-      expect { Yoti::DynamicSharingService.create_share_url scenario }.to raise_error(
-        Yoti::DynamicSharingService::INVALID_DATA
-      )
+    before(:context) do
+      stub_api_requests_v1(:any, 'profile_error', '[a-zA-Z]*', 400)
+    end
+    it 'raises an invalid data error' do
+      expect { Yoti::DynamicSharingService.create_share_url scenario }
+        .to raise_error(
+          Yoti::DynamicSharingService::InvalidDataError
+        )
     end
   end
 
   context 'when the app id is not found' do
-    it 'returns an application not found error' do
-      expect { Yoti::DynamicSharingService.create_share_url scenario }.to raise_error(
-        Yoti::DynamicSharingService::APPLICATION_NOT_FOUND
-      )
+    before(:context) do
+      stub_api_requests_v1(:any, 'profile_error', '[a-zA-Z]*', 404)
+    end
+    it 'raises an application not found error' do
+      expect { Yoti::DynamicSharingService.create_share_url scenario }
+        .to raise_error(
+          Yoti::DynamicSharingService::ApplicationNotFoundError
+        )
+    end
+  end
+
+  context 'when an unknown error occurs' do
+    before(:context) do
+      stub_api_requests_v1(:any, 'profile_error', '[a-zA-Z]*', 500)
+    end
+    it 'raises a generic exception' do
+      expect { Yoti::DynamicSharingService.create_share_url scenario }
+        .to raise_error(
+          Yoti::DynamicSharingService::UnknownHTTPError
+        )
     end
   end
 end

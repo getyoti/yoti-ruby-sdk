b296cf211a6f03e838f5060890f4388d9367d158
Emma Smith <Emma.Smith@Yoti.com>
2019-09-24 11:55:07 +0100

    SDK-726: Add Dynamic Scenario example to rails example project

--- examples/rails/app/controllers/yoti_controller.rb
+++ examples/rails/app/controllers/yoti_controller.rb
@@ -4,6 +4,22 @@ class YotiController < ApplicationController
     @yoti_client_sdk_id = ENV['YOTI_CLIENT_SDK_ID']
   end
 
+  def dynamic_share
+    @yoti_client_sdk_id = ENV['YOTI_CLIENT_SDK_ID']
+
+    scenario = Yoti::DynamicSharingService::DynamicScenario.builder.with_policy(
+      Yoti::DynamicSharingService::DynamicPolicy.builder
+        .with_full_name
+        .with_age_over(18)
+        .with_pin_auth
+        .build
+    ).with_callback_endpoint('/profile').build
+
+    @share = Yoti::DynamicSharingService.create_share_url(scenario)
+    puts "!DEBUG! #{@share}"
+    @share_url = @share.share_url
+  end
+
   def profile
     one_time_use_token = params[:token]
     yoti_activity_details = Yoti::Client.get_activity_details(one_time_use_token)

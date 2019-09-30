aa748fcd71d9cf0c9db3f984ae85a086f3ed723a
Emma Smith <Emma.Smith@Yoti.com>
2019-09-30 16:19:47 +0100

    SDK-726: Add parametised tests for latitude, radius and uncertainty

--- spec/yoti/dynamic_share_service/extension/location_constraint_extension_spec.rb
+++ spec/yoti/dynamic_share_service/extension/location_constraint_extension_spec.rb
@@ -39,7 +39,7 @@ describe 'Yoti::DynamicSharingService::LocationConstraintExtension' do
                 .builder
             end
             it 'throws an exception' do
-              expect { builder.with_latitude(num) }.to raise_error
+              expect { builder.with_latitude(num) }.to raise_error ArgumentError
             end
           end
         end
@@ -47,38 +47,95 @@ describe 'Yoti::DynamicSharingService::LocationConstraintExtension' do
     end
 
     describe '.with_longitude' do
-      let :ext do
-        Yoti::DynamicSharingService::LocationConstraintExtension
-          .builder
-          .with_longitude(75)
-          .build
+      context 'with valid longitude' do
+        [180, 90, 50, 3.0, -90, -180].each do |num|
+          context num.to_s do
+            let :ext do
+              Yoti::DynamicSharingService::LocationConstraintExtension
+                .builder
+                .with_longitude(num)
+                .build
+            end
+            it 'sets a longitude' do
+              expect(ext.content[:expected_device_location][:longitude]).to eql num
+            end
+          end
+        end
       end
-      it 'sets a longitude' do
-        expect(ext.content[:expected_device_location][:longitude]).to eql 75
+      context 'with invalid longitude' do
+        [181, 180.01, 190, -190, 'a string'].each do |num|
+          context num.to_s do
+            let :builder do
+              Yoti::DynamicSharingService::LocationConstraintExtension
+                .builder
+            end
+            it 'throws an exception' do
+              expect { builder.with_longitude(num) }.to raise_error ArgumentError
+            end
+          end
+        end
       end
     end
 
     describe '.with_radius' do
-      let :ext do
-        Yoti::DynamicSharingService::LocationConstraintExtension
-          .builder
-          .with_radius(60)
-          .build
+      context 'with valid radius' do
+        [0, 3.0, 1e-3, 1e3].each do |num|
+          context num.to_s do
+            let :ext do
+              Yoti::DynamicSharingService::LocationConstraintExtension
+                .builder
+                .with_radius(num)
+                .build
+            end
+            it 'sets a radius' do
+              expect(ext.content[:expected_device_location][:radius]).to eql num
+            end
+          end
+        end
       end
-      it 'sets a radius' do
-        expect(ext.content[:expected_device_location][:radius]).to eql 60
+      context 'with invalid radius' do
+        [-1, -1e-6, -1e3, -2.5, 'a string'].each do |num|
+          context num.to_s do
+            let :builder do
+              Yoti::DynamicSharingService::LocationConstraintExtension
+                .builder
+            end
+            it 'throws an exception' do
+              expect { builder.with_radius(num) }.to raise_error ArgumentError
+            end
+          end
+        end
       end
     end
 
     describe '.uncertainty' do
-      let :ext do
-        Yoti::DynamicSharingService::LocationConstraintExtension
-          .builder
-          .with_uncertainty(30)
-          .build
+      context 'with valid uncertainty' do
+        [0, 3.0, 1e-3, 1e3].each do |num|
+          context num.to_s do
+            let :ext do
+              Yoti::DynamicSharingService::LocationConstraintExtension
+                .builder
+                .with_uncertainty(num)
+                .build
+            end
+            it 'sets an uncertainty' do
+              expect(ext.content[:expected_device_location][:uncertainty]).to eql num
+            end
+          end
+        end
       end
-      it 'sets an uncertainty' do
-        expect(ext.content[:expected_device_location][:uncertainty]).to eql 30
+      context 'with invalid uncertainty' do
+        [-1, -1e-6, -1e3, -2.5, 'a string'].each do |num|
+          context num.to_s do
+            let :builder do
+              Yoti::DynamicSharingService::LocationConstraintExtension
+                .builder
+            end
+            it 'throws an exception' do
+              expect { builder.with_uncertainty(num) }.to raise_error ArgumentError
+            end
+          end
+        end
       end
     end
   end

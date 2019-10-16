aafad0eeaa15cdd900ddfd54fd264fc9019825dd
Emma Smith <Emma.Smith@Yoti.com>
2019-10-16 11:09:36 +0100

    SDK-1177: Adding full coverage spec test suite to Sandbox SDK

--- spec/yoti/sandbox/sandbox_anchor_spec.rb
+++ spec/yoti/sandbox/sandbox_anchor_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'yoti/sandbox'
+require 'spec_helper'
 
 describe 'Sandbox::Anchor' do
   let :value do
@@ -18,6 +18,15 @@ describe 'Sandbox::Anchor' do
     end
     it 'creates a source anchor' do
       expected = '{"type":"SOURCE","value":"value","sub_type":"subtype","timestamp":1215302400}'
+      expect(anchor.to_json).to eql expected
+    end
+  end
+  describe '.verifier' do
+    let :anchor do
+      Sandbox::Anchor.verifier(value, sub_type: subtype, timestamp: timestamp)
+    end
+    it 'creates a verifier anchor' do
+      expected = '{"type":"VERIFIER","value":"value","sub_type":"subtype","timestamp":1215302400}'
       expect(anchor.to_json).to eql expected
     end
   end

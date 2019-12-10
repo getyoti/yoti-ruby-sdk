e120822f043bc8b9176fc4324b5737420024e8e7
Emma Smith <Emma.Smith@Yoti.com>
2019-12-10 10:15:20 +0000

    SDK-1263: Refactor ThirdPartyAttributeExtension to match other extensions
    Add unit tests to verify the json produced by building a
    ThirdPartyAttributeExtension

--- lib/yoti/dynamic_share_service/extension/thirdparty_attribute_extension.rb
+++ lib/yoti/dynamic_share_service/extension/thirdparty_attribute_extension.rb
@@ -2,8 +2,7 @@
 
 module Yoti
   module DynamicSharingService
-    class ThirdPartyAttributeExtension
-      THIRD_PARTY_ATTRIBUTE = 'THIRD_PARTY_ATTRIBUTE'
+    class ThirdPartyAttributeExtensionBuilder
       def initialize
         @expiry_date = nil
         @definitions = []
@@ -21,18 +20,38 @@ module Yoti
         self
       end
 
+      def build
+        extension = ThirdPartyAttributeExtension.new
+        extension.instance_variable_get(:@content)[:expiry_date] = @expiry_date
+        extension.instance_variable_get(:@content)[:definitions] = @definitions
+        extension
+      end
+    end
+
+    class ThirdPartyAttributeExtension
+      EXTENSION_TYPE = 'THIRD_PARTY_ATTRIBUTE'
+
+      attr_reader :content
+      attr_reader :type
+
+      def initialize
+        @content = {}
+        @type = EXTENSION_TYPE
+      end
+
       def as_json(*_args)
         {
-          type: THIRD_PARTY_ATTRIBUTE,
-          content: {
-            expiry_date: @expiry_date,
-            definitions: @definitions
-          }
+          type: @type,
+          content: @content
         }
       end
 
       def to_json(*_args)
         as_json.to_json
+      end
+
+      def self.builder
+        ThirdPartyAttributeExtensionBuilder.new
       end
     end
   end

4eb75416a7c55c01cbe1f423d7d5525ae5485d95
Emma Smith <52929061+emmas-yoti@users.noreply.github.com>
2019-12-11 17:42:18 +0000

    SDK-1263: Add Thirdparty Attribute Extension (#75)
    * SDK-1263: Add Thirdparty Attribute Extension
    
    * SDK-1263: Refactor ThirdPartyAttributeExtension to match other extensions
    
    Add unit tests to verify the json produced by building a
    ThirdPartyAttributeExtension

---
+++ spec/yoti/dynamic_share_service/extension/thirdparty_attribute_extension_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Yoti::DynamicSharingService::ThirdPartyAttributeExtension' do
+  describe '.to_json' do
+    let :expiry_date do
+      DateTime.new(2006, 4, 3, 2, 30, 50)
+    end
+    let :attr_name do
+      'AttributeName'
+    end
+    let :ext do
+      Yoti::DynamicSharingService::ThirdPartyAttributeExtension
+        .builder
+        .with_expiry_date(expiry_date)
+        .with_definitions(attr_name)
+        .build
+    end
+    it 'marshals into json' do
+      expected = '{"type":"THIRD_PARTY_ATTRIBUTE","content":{"expiry_date":"2006-04-03T02:30:50.000+00:00","definitions":[{"name":"AttributeName"}]}}'
+      expect(ext.to_json).to eql expected
+    end
+  end
+end

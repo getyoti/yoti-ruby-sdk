460f634da3e4fe93dc272941b991ded9c3543302
David Grayston <davidgrayston@googlemail.com>
2020-05-20 23:01:33 +0100

    SDK-1302: Remove unneeded environment variables from demo app

--- examples/sinatra/app.rb
+++ examples/sinatra/app.rb
@@ -10,8 +10,6 @@ require 'yoti'
 Yoti.configure do |config|
   config.client_sdk_id = ENV['YOTI_CLIENT_SDK_ID']
   config.key_file_path = ENV['YOTI_KEY_FILE_PATH']
-  config.api_url = ENV['YOTI_API_URL'] ||= 'https://api.yoti.com'
-  config.api_port = ENV['YOTI_API_PORT'] ||= '443'
 end
 
 get '/' do
